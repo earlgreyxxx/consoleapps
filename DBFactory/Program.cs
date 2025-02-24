a9e984acab1754a49343cf8b9483343a804ce85c
Kenji Nakagawa <nakagawa@mars.dti.ne.jp>
2025-02-24 21:30:45 +0900

    add project DBFactory

---
+++ DBFactory/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System.Data.Common;
+
+namespace DBFactory
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      DbProviderFactories.RegisterFactory("Microsoft.Data.SqlClient", SqlClientFactory.Instance);
+      //var factory = DbProviderFactories.GetFactory("System.Data.SqlClient");
+      var fc = SqlClientFactory.Instance;
+      //var rows = providers.Rows.Cast<DataRow>().Select(row => 
+
+
+      Console.WriteLine(fc);
+    }
+  }
+}
